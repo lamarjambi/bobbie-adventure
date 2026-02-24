551d14ad5391c4958d3efd7e2ddf8a853e25e403
Lamar Jambi <lmj8416@nyu.edu>
2026-02-23 22:01:39 -0500

    code for animation

--- bobbie-adventure/Assets/Scripts/Player.cs
+++ bobbie-adventure/Assets/Scripts/Player.cs
@@ -1,16 +1,43 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Movement")]
+    [SerializeField] private float moveSpeed = 5f;
+
+    [Header("Animation")]
+    [SerializeField] private Animator animator;
+
+    private Rigidbody2D rb;
+    private Vector2 input;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0f;
+        rb.freezeRotation = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        // normalize for diagonal movement
+        input.Normalize();
+
+        // animation portion
+        if (input != Vector2.zero)
+        {
+            animator.SetBool("isWalking", true);
+        } else
+        {
+            animator.SetBool("isWalking", false);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = input * moveSpeed;
     }
 }
